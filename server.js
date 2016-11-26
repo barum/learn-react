af8c2a4c550f842562d5a45b46b396d92251a48d
bahram <bsamani@samani.com>
2016-11-26 17:50:35 -0500

    added bundle.js

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ var express = require('express');
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(functio (req, res, next) {
-    if (req.headers['x-forwaded-porto']=== 'http') {
-        next();
-    
-    } else {
-        res.redirect('http://'+ req.hostname + req.url) ;
-    
-    }
-});
+// app.use(function (req, res, next) {
+//     if (req.headers['x-forwaded-porto']=== 'http') {
+//         next();
+//
+//     } else {
+//         res.redirect('http://'+ req.hostname + req.url) ;
+//
+//     }
+// });
 
 app.use(express.static('public'));
 
-app.listen(port, function () {
-    console.log('Express Server is up on port 3000');
+app.listen(PORT, function () {
+    console.log('Express Server is up on ' + PORT);
 });
