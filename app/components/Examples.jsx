43c6f854f9f3b3938f725cb7911147948ba7062c
bahram <bsamani@samani.com>
2016-11-27 18:34:14 -0500

    columns

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>WHere are some example locations to try tout!</p>
+      <ol>
+        <li>
+          <Link to='/?location=Tallahassee'>Tallahassee, FL</Link>
+        </li>
+        <li>
+          <Link to='/?location=Orlando'>Orlando, FL</Link>
+        </li>
+      </ol>
     </div>
   )
 };
