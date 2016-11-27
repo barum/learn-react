8ae29b751afb8f73834783b2df5876c9333a4636
bahram <bsamani@samani.com>
2016-11-27 15:03:34 -0500

    copy from andrew

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-/**
- * Created by bahram on 11/20/2016.
- */
-
 var express = require('express');
 
-// create app
-
+// Create our app
 var app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-    if (req.headers['x-forwaded-porto']=== 'http') {
-        next();
-
-    } else {
-        res.redirect('http://'+ req.hostname + req.url) ;
-
-    }
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-    console.log('Express Server is up on ' + PORT);
+  console.log('Express server is up on port ' + PORT);
 });
